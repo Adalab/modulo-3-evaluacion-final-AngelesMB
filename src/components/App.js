15d7df9ff69674263a5268772ea181567a8e454d
AngelesMB <angeles.martinezbarrio@gmail.com>
2022-01-25 14:37:14 +0100

    Add starter kit content

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import "../styles/App.css";
+// import { useEffect, useState } from "react";
+// import ls from "../services/localStorage";
+// import callToApi from "../services/api";
+// import PropTypes from 'prop-types';
+
+function App() {
+
+
+  return (
+    <div>
+      <h1>Harry Potter</h1>
+    </div>
+  );
+}
+
+export default App;
