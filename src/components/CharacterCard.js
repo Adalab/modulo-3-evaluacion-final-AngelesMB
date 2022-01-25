288e22f64e2ba63bbf667b07e0ad2bae0ff13e1e
AngelesMB <angeles.martinezbarrio@gmail.com>
2022-01-25 17:29:11 +0100

    Add image and image placeholder. Translate species.

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,32 @@
 const CharacterCard = ({ eachCharacter }) => {
-  return eachCharacter.name;
+  const translateSpecies = (species) => {
+    switch (species) {
+      case "human":
+        species = "Humano";
+        break;
+      case "half-giant":
+        species = "Medio gigante";
+        break;
+      case "werewolf":
+        species = "Hombre lobo";
+        break;
+      case "ghost":
+        species = "Fantasma";
+        break;
+      default:
+        species = "No conocida";
+    }
+    return species;
+  };
+  const checkImageUrl = (image) => (image === "" ? "https://via.placeholder.com/210x295/ffffff/666666/?text=HarryPotter" : image);
+
+  return (
+    <article>
+      <img src={checkImageUrl(eachCharacter.image)} alt={eachCharacter.name} />
+      <h3>{eachCharacter.name}</h3>
+      <p>{translateSpecies(eachCharacter.species)}</p>
+    </article>
+  );
 };
 
 export default CharacterCard;
