1ad69891e0fffbfb8e329dd5daa8817c93aa78a0
AngelesMB <angeles.martinezbarrio@gmail.com>
2022-01-26 09:17:24 +0100

    Finish localStorage for data and houseFilter

--- src/components/App.js
+++ src/components/App.js
@@ -13,53 +13,30 @@ import Header from "./Header";
 // import PropTypes from 'prop-types';
 
 function App() {
-  // DEFAULT PROPS
-  // Input.defaultProps = {
-  //   inputType: 'text'
-  // };
 
-  // PROP TYPES
-  // Input.propTypes = {
-  //   id: PropTypes.string,
-  //   labelText: PropTypes.string.isRequired,
-  //   inputType: PropTypes.string,
-  //   inputName: PropTypes.string.isRequired,
-  //   inputPlaceholder: PropTypes.string,
-  //   inputValue: PropTypes.string,
-  //   handleChange: PropTypes.func.isRequired,
-  // };
-  // PROP TYPES para OBJETOS
-  // MyComponent.propTypes = {
-  //   data: PropTypes.shape({
-  //     title: PropTypes.string,
-  //     id: PropTypes.string.isRequired,
-  //   }),
-  // };
-  // PROP TYPES para ARRAYS
-  // MyComponent.propTypes = {
-  //   myArrProp: PropTypes.arrayOf(PropTypes.number),
-  // };
 
   // states
   const [data, setData] = useState(ls.get("data", []));
   const [characterFilter, setCharacterFilter] = useState("");
-  const [houseFilter, setHouseFilter] = useState("gryffindor");
+  const [houseFilter, setHouseFilter] = useState(
+    ls.get("houseFilter", "gryffindor")
+  );
 
   const URL = "http://hp-api.herokuapp.com/api/characters/house/";
 
   // effect ls
   useEffect(() => {
     ls.set("data", data);
-  }, [data]);
+    ls.set("houseFilter", houseFilter);
+  }, [data, houseFilter]);
 
   // effect api
   useEffect(() => {
-    // if (data.length === 0 || data[0].house !== houseFilter) {
-    // if (data.length === 0){
-    callToApi(URL, houseFilter).then((response) => {
-      setData(response);
-    });
-    // }
+    if (data.length === 0 || data[0].house.toLowerCase() !== houseFilter) {
+      callToApi(URL, houseFilter).then((response) => {
+        setData(response);
+      });
+    }
   }, [houseFilter]);
 
   const updateFilter = (obj) => {
@@ -95,7 +72,11 @@ function App() {
       <main className="main">
         <Switch>
           <Route exact path="/">
-            <form action="" className="main__form" onSubmit={(ev) => ev.preventDefault()}>
+            <form
+              action=""
+              className="main__form"
+              onSubmit={(ev) => ev.preventDefault()}
+            >
               <CharacterFilter
                 characterFilter={characterFilter}
                 updateFilter={updateFilter}
