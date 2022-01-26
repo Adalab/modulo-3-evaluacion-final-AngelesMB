324a782c1233ff9d0ca8d755751a8ad5593e6a81
AngelesMB <angeles.martinezbarrio@gmail.com>
2022-01-26 17:27:15 +0100

    Add h1 to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ const Header = () => {
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="Harry Potter logo" />
+      <h1 className="header__title">Buscador de personajes de Harry Potter</h1>
     </header>
   );
 };
